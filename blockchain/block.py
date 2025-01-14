142bdfbe58f6d008735264a8de8eaecf2ad86ae3
carvalhoandre <andre_carvalho0@live.com>
2025-01-14 14:30:58 -0300

    feature: base project and class Block

---
+++ blockchain/block.py
@@ -0,0 +1,17 @@
+import hashlib
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def __str__(self):
+        return f"Block(index={self.index}, hash={self.hash}, previous_hash={self.previous_hash}, data={self.data}, nonce={self.nonce})"
