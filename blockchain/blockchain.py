65bc844d9d140fe4942cef66870f886d8e4c5f1d
carvalhoandre <andre_carvalho0@live.com>
2025-01-14 14:40:13 -0300

    feat: Blockchain class

---
+++ blockchain/blockchain.py
@@ -0,0 +1,40 @@
+import time
+
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
+
+    @staticmethod
+    def create_genesis_block():
+        return Block(0, "0", time.time(), "Genesis Block")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = self.proof_of_work(new_block)
+        self.chain.append(new_block)
+
+    def proof_of_work(self, block):
+        while not block.hash.startswith("0" * self.difficulty):
+            block.nonce += 1
+            block.hash = block.calculate_hash()
+
+        return block.hash
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
