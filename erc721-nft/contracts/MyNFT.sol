e5755fc328329c4f8a0483e57c3954259fffae0d
carvalhoandre <andre_carvalho0@live.com>
2025-01-30 20:19:27 -0300

    feat: created ERC-721 contract

---
+++ erc721-nft/contracts/MyNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("MyNFTCollection", "MNFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = _nextTokenId;
+        _nextTokenId++;
+        _safeMint(recipient, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
